458138c4263f1d30c24e1653fa2ed7a275445eaf
divyeshS81 <divyeshsarvaiya999@gmail.com>
2024-06-26 18:55:16 +0530

    add footer section in home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import Nav from './assets/components/Nav'
 import Rout from './assets/components/Rout'
-
+import Footer from './assets/components/Footer'
 
 
 const App = () => {
@@ -11,6 +11,7 @@ const App = () => {
     <BrowserRouter>
       <Nav />
       <Rout />
+      <Footer />
     </BrowserRouter>
    </>
   )
